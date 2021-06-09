26b0d9387af988bc442066f351a9ef66d5f52e74
Paul Krupski <pjkrupski@gmail.com>
2021-06-09 17:24:00 -0400

    fixed missing code

--- RunningSum.java
+++ RunningSum.java
@@ -1,5 +1,26 @@
+public class RunningSum {
 
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 
-public static void main(String [] args){
-    System.out.println("test");
+    public int[] runningSumBadSpace(int[] nums) {
+        int sum = 0;
+        int[] sums = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sums[i] = sum;
         }
+        return sums;
+    }
+
+    // One less iteration of for loop
+    public int[] runningSumGoodSpace(int[] nums) {
+
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
