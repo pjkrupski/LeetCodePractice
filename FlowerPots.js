029bc0a7b72503d0926b4a0397429915be2471fa
Paul Krupski <pjkrupski@gmail.com>
2022-01-18 17:11:47 -0500

    first js problem!

---
+++ FlowerPots.js
@@ -0,0 +1,89 @@
+/*
+   Problem Statement
+   
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+ 
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+
+*/
+
+
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let open = 0;
+    
+    //edge case
+    if(flowerbed.length == 1 && flowerbed[0] == 0){
+        return true;
+    }
+    
+    for(let i = 0; i<flowerbed.length; i++){        
+        if(flowerbed[i] == 0){
+           //Check neighbors if not edge case
+            if(i == 0 && flowerbed.length > 1){
+                if(flowerbed[1] == 0){
+                    flowerbed[0] = 1;
+                    console.log("planted at ", i);
+                    open++;
+                }                
+            }else if(i == flowerbed.length - 1 && flowerbed.length >=3){
+                if(flowerbed[flowerbed.length - 2] == 0){
+                    flowerbed[flowerbed.length - 1] == 1;
+                    console.log("planted at ", i);
+                    open++;
+                }                            
+            }else{
+                if(flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
+                    flowerbed[i] = 1;
+                    console.log("planted at ", i);
+                    open++;
+                }
+            }
+        }
+         if(open >= n){
+        return true;
+    }
+}
+    return false;
+};
+
+/*
+  Test Cases
+[1,0,0,0,1]
+1
+[0,0,0,0]
+3
+[1,0,0,0,1]
+1
+[0,0]
+1
+[0]
+1
+[1]
+1
+[1,0,0,0,1,0,0]
+2
+
+*/
