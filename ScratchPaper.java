c18c147dfcbb670ee8988b17ddbf9a0cff05c0f9
Paul Krupski <pjkrupski@gmail.com>
2022-05-15 23:55:01 +0900

    notes added

--- ScratchPaper.java
+++ ScratchPaper.java
@@ -1,16 +1,43 @@
 import java.util.*;
 
 public class ScratchPaper {
+    static List<List<Character>> ans;
+    static List<Character> current;
     public static void main(String [] args){
         Set<String> set = new HashSet<String>();
-        char[] array = {'a', 'b', 'c', 'd', 'e'};
+        char[] array = {'a', 'b', 'c','d'};
+        String[] letters = {"a","b","c","d"};
         String str = "abc";
         //System.out.println(powerSet(array, array.length).toString());
-        set.addAll(powerSet(array));
+        //set.addAll(powerSet(array));
         //set.addAll(combinations(array));
-        System.out.println(set.toString());
-        //System.out.println(permutations(array).size());
+        //System.out.println(set.toString());
+       // System.out.println(permutations(array));
+        List<List<Character>> temp = combination(array,array.length, 3);
+        for(int i =0; i < temp.size(); i++){
+            System.out.println(temp.get(i).toString());
+        }
     }
+
+    //https://leetcode.com/problems/combinations/discuss/2030259/Easy-Faster-Efficient-Java-Soln
+    static List<List<Character>> combination(char[] chars, int n, int k) {
+        ans = new ArrayList<>();
+        current = new ArrayList<>();
+        combinationHelper(chars, 1, n, k);
+        return ans;
+    }
+    static void combinationHelper(char[] chars, int idx, int n, int k){
+        if(k == 0){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+        for(int i = idx; i <= n; i++){
+            current.add(chars[i-1]);
+            combinationHelper(chars, i + 1, n, k - 1);
+            current.remove(current.size() - 1);
+        }
+    }
+    
 
     static List<String> powerSet(char [] chars){
         StringBuilder sb;
@@ -30,25 +57,6 @@ public class ScratchPaper {
         return lst;
     }
 
-    static List<String> combinations(char [] chars, int n){
-        StringBuilder sb;
-        List<String> lst = new ArrayList<>();
-        /*set_size of power set of a set
-        with set_size n is (2**n -1)*/
-        int  j;
-        for(int i = 0; i < Math.pow(2, chars.length); i++) {
-            sb = new StringBuilder();
-            for (j = 0; j < chars.length; j++) {
-                if ((i & (1 << j)) > 0) {
-                    sb.append((chars[j]));
-                }
-            }
-            if(sb.length() == n) {
-                lst.add(sb.toString());
-            }
-        }
-        return lst;
-    }
 
     // Iterative function to generate all permutations of a string in Java
     // using Collections
